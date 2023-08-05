13f1cdd7e7a0e353993f6535c83913fff35d62bb
Garit Ahsanut <garitahsanut22@gmail.com>
2023-08-05 17:38:47 +0700

    Create weird-algorithms.cpp

---
+++ CSES/Introductory Problems/weird-algorithms.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	long long n;
+	cin >> n;
+	
+	while (n != 1) {
+		cout << n << " ";
+		if ( n % 2 != 0) {
+			n = 3 * n + 1;
+		} else {
+			n /= 2;
+		}
+	}
+	
+	cout << 1 << '\n';
+	
+	return 0;
+}
